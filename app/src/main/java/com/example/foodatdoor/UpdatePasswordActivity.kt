055bd1bcefb3bc59f0571d33e259ae37e6d8e2c4
Anju1415 <anjuannu1415@gmail.com>
2020-07-17 17:25:40 +0530

    update pass

--- app/src/main/java/com/example/foodatdoor/UpdatePasswordActivity.kt
+++ app/src/main/java/com/example/foodatdoor/UpdatePasswordActivity.kt
@@ -1,11 +1,65 @@
 package com.example.foodatdoor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+/** to update password if user want **/
 class UpdatePasswordActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var passwordEt: EditText
+    private lateinit var changePasswordBtn: Button
+    private lateinit var back: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_password)
+
+        auth = FirebaseAuth.getInstance()
+
+        passwordEt = findViewById(R.id.password_edt_text)
+
+        changePasswordBtn = findViewById(R.id.reset_pass_btn)
+        back = findViewById(R.id.back_btn)
+
+        back.setOnClickListener {
+            val intent = Intent(this, HomePageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        changePasswordBtn.setOnClickListener {
+            var password: String = passwordEt.text.toString()
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show()
+            } else {
+                if (password.length < 6) {
+                    Toast.makeText(
+                        this,
+                        "Password too short! , enter minimum 6 characters",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                auth.currentUser?.updatePassword(password)
+                    ?.addOnCompleteListener(this, OnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Password changes successfully", Toast.LENGTH_LONG)
+                                .show()
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this, "password not changed", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    })
+            }
+        }
     }
 }
