94781958006a79b3247328e2b8a5b240eb764906
Anju Kumari <anjuannu1415@gmail.com>
2020-08-11 01:38:13 +0530

    razorPay integration

---
+++ app/src/main/java/com/example/foodatdoor/RozorPayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.foodatdoor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RozorPayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rozor_pay)
+    }
+}
