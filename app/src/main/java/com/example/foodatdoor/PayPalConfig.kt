17162c5485ef2b954c7b82c116e2f46acde5857d
Anju Kumari <42924677+Anju1415@users.noreply.github.com>
2020-09-25 22:33:33 +0530

    Update PayPalConfig.kt

--- app/src/main/java/com/example/foodatdoor/PayPalConfig.kt
+++ app/src/main/java/com/example/foodatdoor/PayPalConfig.kt
@@ -1,5 +1,5 @@
 package com.example.foodatdoor
 
 object PayPalConfig {
-    const val PAYPAL_CLIENT_ID = "AbIE8gjIF2QI5R0F1zITBNoMyGczEEdihJGbuTILEwinSIliwHfoPqmDi38uNiIRf5_MqHbbAAit5_w9"
+    const val PAYPAL_CLIENT_ID = "YOUR PAYPAL CLIENT ID"
 }
